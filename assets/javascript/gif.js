9811d722f932f61e2180a12e76ab66081fad1d23
unknown <sherman.jonathan.r@gmail.com>
2019-03-21 22:11:24 -0700

    functions and on click working/

---
+++ assets/javascript/gif.js
@@ -0,0 +1,90 @@
+var sports = ["hockey", "snowboarding","golf"];
+
+
+        function createButtons() {
+
+            $("#buttons").empty();
+
+            sports.forEach(function (spt, index) {
+                console.log(index);
+
+                var btn = $("<button  class=rounded-pill>>");
+
+                btn.addClass("newSport");
+
+                btn.attr("data-sport", spt);
+
+                btn.text(spt);
+
+                $("#buttons").append(btn);
+            });
+        };
+
+        $("#new-sport").on("click", function (e) {
+
+            e.preventDefault();
+
+            var newSport = $("#input-area").val().trim();
+
+            $("#input-area").val("");
+
+            sports.push(newSport);
+
+            createButtons();
+        })
+
+        createButtons();
+
+
+        $("#buttons").on("click", "button", function display() {
+            var sport = $(this).attr("data-sport");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=LJ2XOTPs4vG2CFF25DCvU86vJIQqPiiE&q=" + sport + "&limit=10";
+
+            console.log(queryURL);
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+
+                var results = response.data;
+                console.log(results);
+
+                results.forEach(function (result, index) {
+                    console.log(index);
+
+                    var gifDiv = $("<div>");
+
+                    var rating = result.rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    var giphy = $("<img>");
+                    giphy.attr("src", result.images.fixed_height_still.url);
+                    giphy.attr("data-still", result.images.fixed_height_still.url);
+                    giphy.attr("data-animate", result.images.fixed_height.url);
+                    giphy.attr("data-state", "still");
+                    giphy.addClass("start");
+
+                    gifDiv.prepend(p, giphy);
+
+                    $("#where-gifs-go").prepend(gifDiv);
+
+                });
+
+                // to do: create dynamic elemets for gifs
+
+                $(".start").on("click", function () {
+
+                    var move = $(this).attr("data-state");
+
+                    if (move === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+                    };
+                })
+            });
+        })
